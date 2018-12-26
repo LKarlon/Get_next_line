d98eb17528729cb1ba96e73ed8fb7b47a8df25c7
Lysono maar Karlon stark <lkarlon-@e3r5p12.21-school.ru>
2018-12-26 17:43:38 +0300

    add some changes

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,27 @@
 /*   By: lkarlon- <lkarlon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 16:05:45 by lkarlon-          #+#    #+#             */
-/*   Updated: 2018/12/24 19:54:11 by lkarlon-         ###   ########.fr       */
+/*   Updated: 2018/12/26 17:28:21 by lkarlon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
+
+int			node_del(t_remain **dat)
+{
+	t_remain *save;
+
+	save = *dat;
+	if (*dat)
+	{
+		free((*dat)->content);
+		(*dat)->next = NULL;
+		*dat = save->next;
+		ft_memdel((void**)&save);
+	}
+	return (0);
+}
 
 char		*ft_join(char *s1, char *s2, size_t main, ssize_t last)
 {
@@ -34,8 +49,7 @@ char		*ft_join(char *s1, char *s2, size_t main, ssize_t last)
 		i++;
 		s2++;
 	}
-	if (s1)
-		ft_memdel((void**)&s1);
+	ft_memdel((void**)&s1);
 	ft_memdel((void**)&save);
 	return (new_str);
 }
@@ -53,7 +67,7 @@ char		*sort(char *str, t_remain *dat, size_t count)
 			buf = ft_strnew(i);
 			ft_memcpy(buf, str, i);
 			dat->ret_size = i++;
-			ft_memcpy(&dat->content[0], &str[i], --count);
+			ft_memmove(&dat->content[0], &str[i], --count);
 			dat->content_size = count;
 			dat->stop = 1;
 			return (buf);
@@ -65,7 +79,7 @@ char		*sort(char *str, t_remain *dat, size_t count)
 	dat->stop = 0;
 	dat->ret_size = i;
 	dat->content_size = count;
-	ft_memcpy(buf, str, i);
+	ft_memmove(buf, str, i);
 	return (buf);
 }
 
@@ -123,6 +137,6 @@ int			get_next_line(const int fd, char **line)
 		m_count += dat->ret_size;
 	}
 	ft_memdel((void**)&buffer);
-	m_count = (!(**line) && !ret ? 0 : 1);
+	m_count = (!(**line) && !ret ? node_del(&dat) : 1);
 	return (ret < 0 ? -1 : m_count);
 }
